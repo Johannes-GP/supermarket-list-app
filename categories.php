1966490f2009c66ba45deddef0dfb0dd99617f12
Johannes Guzman <juancarlos.gp0103@gmail.com>
2021-12-03 18:27:28 -0600

    Added index.php and categories.php with some test images

---
+++ categories.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Categorias</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="card" style="width: 18rem;">
+            <img src="img/carniceria.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">CARNICERIA</p>
+                Photo by <a href="https://unsplash.com/@idelamaza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Iñigo De la Maza</a> on <a href="https://unsplash.com/s/photos/butcher-shop?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="img/farmacia.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">FARMACIA</p>
+                Photo by <a href="https://unsplash.com/@freestocks?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">freestocks</a> on <a href="https://unsplash.com/s/photos/drugstore?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="img/frutas-verduras.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">FRUTAS Y VERDURAS</p>
+                Photo by <a href="https://unsplash.com/@_gemmajade?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">gemma</a> on <a href="https://unsplash.com/s/photos/grocery-store?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
